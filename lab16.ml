9ce9b462a7e77cc3bb2da5330c084fa90986cb79
Hassan Looky <114613998+HLooky@users.noreply.github.com>
2023-04-09 23:26:36 -0400

    Update lab16.ml

--- lab16.ml
+++ lab16.ml
@@ -15,11 +15,9 @@ open Point ;;
 
 (*====================================================================
 Part 1: Function-oriented vehicles 
-
 In this part, you will revisit algebraic data types to model some
 vehicles, implementing functionality in a function-oriented approach,
 with comparison to object-oriented implementations to come in Part 2.
-
 We start with a `vehicle` type that can be a `Bus`, a `Car`, or a
 `Truck`, each constructed of a `point` that represents its current x-y
 position and a `float` that represents the energy stored in the tank
@@ -79,24 +77,18 @@ distance (a `float`), and a direction (an angle in radians represented
 by a `float`), and returns updated information about the vehicle --
 its new position after traveling that distance in that direction, and
 its energy reduced accordingly.
-
 Some important points:
-
    o Your function should return a new `vehicle` with the updated
      position and energy. (Since the `point` and `vehicle` types are
      not mutable, you can't update the information "in place".)
-
    o Calling the function with a negative distance should raise an
      `Invalid_argument` exception.
-
    o Fortunately, you don't need to know how to do the calculation of
      the change in position, since the `Point` module can handle it
      for you; see the `Point.offset` function.
-
    o Assume that a vehicle can only go as far as its energy will carry
      it, so if the `distance` is farther than that, the vehicle will
      go as far as its energy allows and stop with no remaining energy.
-
 ....................................................................*)
 
 let go (veh : vehicle) (distance : float) (angle : float) : vehicle =
@@ -116,7 +108,6 @@ let go (veh : vehicle) (distance : float) (angle : float) : vehicle =
    
 (* At this point, you should be able to model a vehicle's movement
 like this:
-
       # open Lab16 ;;
       # let mach5 = Car ((0., 0.), 5.) ;;   
       val mach5 : Lab16.vehicle = Car ((0., 0.), 5.)
@@ -133,7 +124,6 @@ like this:
 
 (*====================================================================
 Part 2: Object-oriented vehicles
-
 Having implementing a few functions to be performed on vehicles
 defined as an algebraic data type, you might be getting weary of all
 these match statements you need to add. Consider what would happen if
@@ -147,7 +137,6 @@ dozens of instances in your code where you match to the `vehicle`
 variant type. For each of these different locations, perhaps even in
 different files that you have never seen or touched (if working in a
 group), the extra match cases would need to be added.
-
 We named our type `vehicle`. What do vehicles have in common? In our
 case, they have a certain amount of energy, an efficiency with which
 they use that energy, and the ability to move in two dimensional
@@ -158,22 +147,15 @@ structure. We've provided the skeleton of a `vehicle_class` class
 called the algebraic data type `vehicle` and OCaml doesn't allow the
 shared name). Notice the syntax for defining a class, as well as the
 field variables and methods.
-
 The `vehicle_class` class constructor takes several arguments:
-
   capacity -- the maximum amount of energy (in gallons of gas) that
   can be stored in the vehicle's tank/battery
-
   efficiency -- the vehicle's efficiency in mpg
-
   initial_energy -- the vehicle's initial amount of energy
-
   initial_pos -- the initial position of the vehicle
-
 The class has several instance variables to store these values, as
 well as an odometer field (initially 0.) to track the total number of
 miles driven.
-
 (Note that some fields are mutable while some are not. Do you see
 why?) *)
 
@@ -246,7 +228,6 @@ class vehicle_class (capacity: float)
 
 (* At this point, you should be able to model a vehicle's movement
 like this:
-
       # let mach5 = new vehicle_class 5. 30. 5. (0., 0.) ;;   
       val mach5 : Lab16.vehicle_class = <obj>
       # mach5#get_pos ;;
@@ -269,7 +250,6 @@ like this:
   
 (*====================================================================
 Part 3 Inheritance
-
 Let's say that we know certain facts about the energy and efficiency
 corresponding to buses, trucks, and cars as we did above and thus
 would like to conveniently be able to refer to them in more detail
@@ -278,17 +258,14 @@ than just as vehicles, for whatever purpose we may have down the road
 of the types of vehicles, we can make use of code we already wrote in
 the `vehicle_class` definition by taking advantage of inheritance. Try
 this out for yourself below.
-
 ......................................................................
 Exercise 8: Define a `car` class, taking advantage of inheritance by
 inheriting from the `vehicle_class` class.  Objects of the `car` class
 should have the energy efficiency and capacity as in this table:
-
            efficiency  capacity
     Car        30.        100.
     Truck      15.        150.
     Bus        20.        200.
-
 Hint: Because of the power of inheritance, your definition of the car
 class should require only a few lines of code.
 ....................................................................*)
@@ -312,18 +289,13 @@ class truck (initial_energy : float) (initial_pos : point) =
 Exercise 10: Finally, define the `bus` class. Rather than merely
 inherit all the functionality from the vehicle class, we'll add
 additional functionality that makes a bus a bus.
-
 A bus should be able to pick up and drop off passengers. To implement
 this functionality, give your bus class additional instance variables:
-
     `seats` -- the number of seats on the bus, which defines the
        maximum number of passengers it can accommodate
-
     `passengers` -- the number of passengers currently on the bus
-
 (You'll want to think about whether these fields should be mutable or
 not.) 
-
 The class should allow for methods `pick_up` and `drop_off`, which
 both take the number of passengers to perform the action on and return
 a `unit`. Keep in mind you can't drop off more passengers than you
@@ -331,7 +303,6 @@ currently have, so in this case you can just reset your passenger
 count to 0. A bus also offers finite seating (50 seats, in our case)
 and this rule should be enforced as well. So, if 70 people try to
 board the bus at the same time, only the first 50 will be able to.
-
 Furthermore, when a bus goes in for a fill-up, it will behave as a
 vehicle, but first needs to drop off all its passengers. Override the
 `fill` method to implement this functionality.
@@ -360,16 +331,13 @@ class bus (initial_energy : float) (initial_pos : point) (seats : int) =
   end ;;
 
 (* Notice in particular the overriding of the `fill` method.
-
    o We've added dropping off passengers to the `fill` method as per
      the spec, by using an invocation of the `this#drop_off`
      method. (That's better than directly specifying `passengers <- 0`
      for the reasons outlined in the note on exercise 6.)
-
    o We can still make use of the more general `fill` method in the
      super-class, invoking it with `super#fill`. No need to
      reimplement that functionality.
-
    o We've marked the method with `method!` instead of just
      `method`. The extra `!` diacritic tells OCaml that this method
      overrides an inherited method, so that OCaml will generate a
